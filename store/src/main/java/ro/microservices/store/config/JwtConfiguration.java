7a6b145240bd4c44f8542b3e1d944fffb4304b46
Andreea <ifrim.andree@gmail.com>
2017-09-06 16:30:10 +0300

    security added

---
+++ store/src/main/java/ro/microservices/store/config/JwtConfiguration.java
@@ -0,0 +1,44 @@
+package ro.microservices.store.config;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.util.FileCopyUtils;
+
+@Configuration
+public class JwtConfiguration {
+
+	@Autowired
+	private JwtAccessTokenConverter jwtAccessTokenConverter;
+	
+	@Bean
+	@Qualifier("tokenStore")
+	public TokenStore tokenStore() {
+		return new JwtTokenStore(jwtAccessTokenConverter);
+		
+	}
+	
+	@Bean
+	protected JwtAccessTokenConverter jwtTokenEnhancer() {
+		Resource resource = new ClassPathResource("public.cert");
+		JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+		
+		try {
+			String publicKey = new String(FileCopyUtils.copyToByteArray(resource.getInputStream()));
+			converter.setVerifierKey(publicKey);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		
+		return converter;
+	}
+	
+}
---
+++ store/src/main/java/ro/microservices/store/config/ResourceServerConfiguration.java
@@ -0,0 +1,29 @@
+package ro.microservices.store.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    TokenStore tokenStore;
+
+    public void configure (HttpSecurity http) throws Exception {
+    	http.csrf().disable()
+    	.authorizeRequests()
+    	.antMatchers("/v1/**").authenticated();
+    }
+    
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore);
+    }
+
+}
